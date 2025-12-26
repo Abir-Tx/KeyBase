4e71b5ed5db149a50a10d793744c72a28a637e3c
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2025-12-26 21:19:04 +0600

    refactor: Remove automatic table creation from main application file
    - As I am using alembic data migration so this needs to be removed
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.router import router
-from app.db.base import Base
-from app.db.session import engine
 
-# ✅ Dev-only: automatically create tables if they don't exist
-Base.metadata.create_all(bind=engine)
 
 # ✅ FastAPI instance
 app = FastAPI(title="KeyBase API")
