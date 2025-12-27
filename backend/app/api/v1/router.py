834f6c51d20e7c208a3fc27378ed60c80ab9e7dd
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2025-12-27 20:58:50 +0600

    🔧 (backend/app/api/v1/router.py): Add a /health check route for Docker health check
    Add a new route to the router for checking the application's health, which is useful when running the app in a containerized environment like Docker. The route returns an "ok" status when accessed.

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -4,3 +4,8 @@ from app.api.v1 import shortcuts
 router = APIRouter()
 router.include_router(shortcuts.router)
 
+
+# Add a /health check route to the router for docker health check
+@router.get("/health")
+def health():
+    return {"status": "ok"}
