5a1477424642f4ffd0d0556d7fbfbdaa477258a5
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2025-12-27 19:48:33 +0600

    Add the lib folder
    - for the default ignore list the folder was ignored.
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,31 @@
+// src/lib/api.ts
+import axios from "axios";
+
+// The API Connection Base URL | Read it from .env file
+export const API_BASE = process.env.API_BASE || "http://localhost:8001";
+
+export type Shortcut = {
+  id?: number;
+  name: string;
+  keys: string[];
+  app: string;
+  os: string;
+  description?: string;
+  tags: string[];
+};
+
+export const fetchShortcuts = async (): Promise<Shortcut[]> => {
+  const res = await axios.get(`${API_BASE}/shortcuts`);
+  return res.data;
+};
+
+export const addShortcut = async (shortcut: Shortcut) => {
+  const res = await axios.post(`${API_BASE}/shortcuts`, shortcut);
+  return res.data;
+};
+
+export const updateShortcut = async (shortcut: Shortcut) => {
+  if (!shortcut.id) throw new Error("Shortcut ID is required for update");
+  const res = await axios.put(`${API_BASE}/shortcuts/${shortcut.id}`, shortcut);
+  return res.data;
+};
